72dbc4697aa849d5d32e18485d8634010b6a85ee
Theasasin7 <ulises.cruz7@hotmail.com>
2021-12-16 20:14:59 -0500

    Arreglo en caso de que no se escriba edad

--- Clase 2/javascript/script.js
+++ Clase 2/javascript/script.js
@@ -5,6 +5,8 @@ if (name != "" && age >= 21){
     alert ("The user " + name + " is " + age + " years old and is of legal age to drink alcohol.");
 }else if (!name != ""){
     alert ("You need to type your name to continue.");
+}else if (isNaN(age)){
+    alert ("You need to type your age to continue.");
 }else {
     alert ("The user " + name + " is " + age + " years old and is not old enough to consume alcohol.");
 }
