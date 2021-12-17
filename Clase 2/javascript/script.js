7365cd7094fe1d31850d6058fd0939e33828d5d1
Theasasin7 <ulises.cruz7@hotmail.com>
2021-12-16 19:46:02 -0500

    Clase 2 y desafio creados.

---
+++ Clase 2/javascript/script.js
@@ -0,0 +1,10 @@
+let name = prompt("Name:");
+let age = parseInt(prompt("How old are you?"));
+
+if (name != "" && age >= 21){
+    alert ("The user " + name + " is " + age + " years old and is of legal age to drink alcohol.");
+}else if (!name != ""){
+    alert ("You need to type your name to continue.");
+}else {
+    alert ("The user " + name + " is " + age + " years old and is not old enough to consume alcohol.");
+}
